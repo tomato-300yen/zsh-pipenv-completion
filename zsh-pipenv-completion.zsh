06d6285a8b829afb49be0f7f40c861e854a00e1c
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-01 19:08:45 +0900

    zsh-pipenv-competion.zsh: function

--- zsh-pipenv-completion.zsh
+++ zsh-pipenv-completion.zsh
@@ -1,10 +1,16 @@
-COMP_DIR=${ZDOTDIR:-$HOME}/completion
-if [ ! -e COMP_DIR ]; then
-  mkdir COMP_DIR
-fi
-COMP_FILE=$COMP_DIR/_pipenv
-DUMP_FILE=${ZDOTDIR:-$HOME}/.zcompdump
-if [ ! -f COMP_FILE ]; then
-  pipenv --completion > COMPDEF_FILE
-  [ -f DUMP_FILE ] && rm DUMP_FILE
-fi
+function enable_completion {
+  if [ ! type pipenv > /dev/null 2>&1 ]; then
+    return 0
+  fi
+  COMP_DIR=${ZDOTDIR:-$HOME}/completion
+  COMP_FILE=$COMP_DIR/_pipenv
+  DUMP_FILE=${ZDOTDIR:-$HOME}/.zcompdump
+  [ ! -e $COMP_DIR ] && mkdir $COMP_DIR
+
+  if [ ! -f $COMP_FILE ]; then
+    pipenv --completion > $COMPDEF_FILE
+    [ -f $DUMP_FILE ] && rm $DUMP_FILE
+  fi
+}
+
+enable_completion
