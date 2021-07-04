f092e8486b0414e97a3f9279b05cdabee47c2fb5
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-04 11:37:45 +0900

    zsh-pipenv-completion.zsh: lazyloading

--- zsh-pipenv-completion.zsh
+++ zsh-pipenv-completion.zsh
@@ -1,16 +1,6 @@
-function enable_completion {
-  if [ ! type pipenv > /dev/null 2>&1 ]; then
-    return 0
-  fi
-  COMP_DIR=${ZDOTDIR:-$HOME}/completion
-  COMP_FILE=$COMP_DIR/_pipenv
-  DUMP_FILE=${ZDOTDIR:-$HOME}/.zcompdump
-  [ ! -e $COMP_DIR ] && mkdir $COMP_DIR
-
-  if [ ! -e $COMP_FILE ]; then
-    pipenv --completion > $COMP_FILE
-    [ -f $DUMP_FILE ] && rm $DUMP_FILE
-  fi
+function pipenv() {
+  unfunction "$0"
+  echo "enabled pipenv completion"
+  source < (pipenv --completion)
+  $0 "$@"
 }
-
-enable_completion
